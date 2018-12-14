bd96a88af021eda8eea48c45d0066c98e9471cd5
Alexander von Weiss <alexvonweiss@googlemail.com>
2018-12-14 14:59:10 +0100

    add sub component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SubComponentComponent} from 'src/app/sub-component/sub-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'sub',
+  component: SubComponentComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubComponentComponent } from './sub-component/sub-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubComponentComponent
   ],
   imports: [
     BrowserModule,
